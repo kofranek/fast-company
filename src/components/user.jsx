bc5852ba509f38b0b19b93c9f39275fd818a4902
Jiří Kofránek <kofranek@gmail.com>
2021-10-07 04:48:48 +0200

    Почему не срабатывает Click в user:       <td>         <BookMark bookmark={ props.bookmark }                   onClick={ () => console.log('cliked at props._id', props._id) }                   //onClick={()=>props.onToggleBookmark(props._id)}         />       </td>

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -1,51 +1,29 @@
 import React from 'react';
+import BookMark from './bookmark'
 
 const User = (props) => {
-  console.log('user props',props)
-  function iconSelected (bookmark) {
-    //console.log('selected=',selected)
-    if (bookmark) {
-      return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-             className="bi bi-bookmark-fill" viewBox="0 0 16 16">
-          <path
-            d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2z"/>
-        </svg>
-      )
-    } else return (
-
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-bookmark"
-           viewBox="0 0 16 16">
-        <path
-          d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"/>
-      </svg>
-    )
-  }
+  // console.log('user props', props)
 
   return (
     <tr>
       <td>{ props.name }</td>
       <td>
-        {/*{props.qualities[0].name}*/}
+        {/*{props.qualities[0].name}*/ }
         { props.qualities
-        .map(el =>
-               <span
-                 className={ 'badge m-1 bg-' + el.color }
-                 key={ el._id }
-               >{ el.name }</span>) }
+          .map(el =>
+                 <span
+                   className={ 'badge m-1 bg-' + el.color }
+                   key={ el._id }
+                 >{ el.name }</span>) }
       </td>
       <td>{ props.profession.name }</td>
       <td>{ props.completedMeetings }</td>
       <td>{ props.rate } / 5</td>
       <td>
-        <a
-          href="#"
-          // onClick={ () => props.handleSelected(props._id) }
-          //onClick={()=>console.log('item id ',props._id,' with name ',props.name,' will be selected')}
-          onClick = {()=>props.onToggleBookmark(props._id)}
-        >
-          { iconSelected(props.bookmark) }
-        </a>
+        <BookMark bookmark={ props.bookmark }
+                  onClick={ () => console.log('cliked at props._id', props._id) }
+                  //onClick={()=>props.onToggleBookmark(props._id)}
+        />
       </td>
       <td>
         <button
