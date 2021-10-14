d9f27600d4eda06016c8fc4ae9ad3e45aaed6415
Jiří Kofránek <kofranek@gmail.com>
2021-10-14 02:25:58 +0200

    Pagination of data

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,20 +1,33 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import Pagination from './pagination'
 import User from './user'
+import { paginate } from '../utils/paginate'
 
-const Users = ({ users, ...rest }) => {
+const Users = ({ users:allUsers, ...rest }) => {
   console.log('Users rest=', rest)
-  const count = users.length
+  const count = allUsers.length
   const pageSize = 4
-  const [currentPage, setCurrentPage]=useState(1)
-  const handlePageChange = (pageIndex) =>{
+  const [currentPage, setCurrentPage] = useState(1)
+  const handlePageChange = (pageIndex) => {
     console.log('page:', pageIndex)
     setCurrentPage(pageIndex)
   }
+  let users = paginate(allUsers, currentPage, pageSize)
+
+  if (users.length===0) {
+    console.log('currentPage=',currentPage)
+    if (currentPage>1) {
+      handlePageChange(currentPage-1)
+      users = paginate(allUsers, currentPage, pageSize)
+    }
+  }
+
+
   function tableRender () {
     if (count === 0) return
     return (
       <>
+
         <table className={ 'table table-sm' }>
           <thead>
           <tr>
@@ -40,12 +53,12 @@ const Users = ({ users, ...rest }) => {
           )) }
           </tbody>
         </table>
-        <Pagination
-          itemsCount = {count}
-          pageSize = {pageSize}
-          currentPage ={currentPage}
-          onPageChange = {handlePageChange}
 
+        <Pagination
+          itemsCount={ count }
+          pageSize={ pageSize }
+          currentPage={ currentPage }
+          onPageChange={ handlePageChange }
         />
       </>
     )
