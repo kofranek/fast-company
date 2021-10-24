bc4e19baf8bb47ec6b05b8fdfaa294ade3055087
Hollarka <kofranek@gmail.com>
2021-10-24 18:51:49 +0200

    Revert "Practical task # 1 - init"
    This reverts commit 09db25ffb06723427d6ae9437befa093697d0b13.

--- src/API/index.js
+++ src/API/index.js
@@ -1,7 +1,8 @@
-import users from "./fake.api/user.api";
-import professions from "./fake.api/professions.api";
+import * as users from "./fake.api/user.api"
+import professions from "./fake.api/professions.api"
+
 const API = {
     users,
     professions
-};
-export default API;
+}
+export default API
